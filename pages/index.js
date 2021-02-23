ec9bf6ef63cbfb24eb05f48a537a63cfb03f7f4c
Anthony Kuang <kuanganthony@gmail.com>
2021-02-22 16:11:10 -0800

    useEffect()

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [content, setContent] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:5000/')
+      .then((response) => response.json())
+      .then((data) => setContent(data.response));
+  }, []);
+
   return (
     <div>
       <Head>
@@ -8,7 +17,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1>bruh</h1>
+      <h1>{content}</h1>
     </div>
   );
 }
